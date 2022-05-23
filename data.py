edd4d44d2abf72afef63bf8151238d37b61972fc
565353780 <565353780@qq.com>
2022-05-23 18:35:52 +0800

    finish data script

--- data.py
+++ data.py
@@ -4,36 +4,40 @@
 import torch
 from torch_geometric.data import Data
 
-edge_index_format_1 = torch.tensor([[0, 1, 1, 2],
-                                    [1, 0, 2, 1]], dtype=torch.long)
-edge_index_format_2 = torch.tensor([[0, 1],
-                                    [1, 0],
-                                    [1, 2],
-                                    [2, 1]], dtype=torch.long)
+edge_index = torch.tensor([[0, 1, 1, 2],
+                           [1, 0, 2, 1]], dtype=torch.long)
+edge_index_2 = torch.tensor([[0, 1],
+                             [1, 0],
+                             [1, 2],
+                             [2, 1]], dtype=torch.long)
+
 x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
 
-data_1 = Data(x=x, edge_index=edge_index_format_1)
-data_2 = Data(x=x, edge_index=edge_index_format_2.t().contiguous())
+data = Data(x=x, edge_index=edge_index)
+data_2 = Data(x=x, edge_index=edge_index_2.t().contiguous())
 
-print("[INFO][data::]")
-print("\t data created success! two edge_index_format datas are:")
-print(data_1)
-print(data_2)
+print("[INFO]data:")
+print(data)
 print()
 
-print("[INFO][data::]")
-print("\t keys:")
-print(data_1.keys)
+print("[INFO]keys:")
+print(data.keys)
 print()
 
-print("[INFO][data::]")
-print("\t values:")
-print(data_1['x'])
-print()
-
-print("[INFO][data::]")
-for key, item in data_1:
-    print("data_1[" + key + "]:")
+for key, item in data:
+    print("[INFO]data[" + key + "]:")
     print(item)
-print()
+    print(data[key])
+    print()
+
+print('edge_attr' in data)
+print("num_nodes =", data.num_nodes)
+print("num_edges =", data.num_edges)
+print("num_node_features =", data.num_node_features)
+print("has_isolated_nodes =", data.has_isolated_nodes())
+print("has_self_loops =", data.has_self_loops())
+print("is_directed =", data.is_directed())
+
+device = torch.device('cuda')
+data = data.to(device)
 
