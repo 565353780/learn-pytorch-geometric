daf6763fcd11665122bb809060f4337130480f71
565353780 <565353780@qq.com>
2022-05-24 15:39:55 +0800

    update dataset algo

--- dataset.py
+++ dataset.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os.path as osp
+
+import torch
 import torch_geometric.transforms as T
+from torch_geometric.data import Data, InMemoryDataset, Dataset, download_url
 from torch_geometric.datasets import TUDataset, Planetoid, ShapeNet
 
 dataset_root = "/home/chli/chLi/Download/DeepLearning/Dataset/PytorchGeometric/"
@@ -57,4 +61,96 @@ dataset = ShapeNet(root=dataset_root + 'ShapeNet', categories=['Airplane'],
                    pre_transform=T.KNNGraph(k=6),
                    transform=T.RandomTranslate(0.01))
 print("augment, dataset[0] =", dataset[0])
+
+print("====Custom Dataset====")
+
+class CustomInMemoryDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return ['file_name_1', 'file_name_2']
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    def download(self):
+        url = None
+        if url is None:
+            return
+        download_url(url, self.raw_dir)
+        return
+
+    def process(self):
+        data_list = [Data(), Data()]
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+class CustomDataset(Dataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+
+    @property
+    def raw_file_names(self):
+        return ['file_name_1', 'file_name_2']
+
+    @property
+    def processed_file_names(self):
+        return ['data_1.pt', 'data_2.pt']
+
+    def download(self):
+        url = None
+        if url is None:
+            return
+        path = download_url(url, self.raw_dir)
+        print("[INFO][CustomDataset::download]")
+        print("path =", path)
+        return
+
+    def process(self):
+        idx = 0
+        for raw_path in self.raw_paths:
+            print("[INFO][CustomDataset::process]")
+            print("raw_path =", raw_path)
+
+            data = Data()
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            torch.save(data, osp.join(self.processed_dir,
+                                      "data_" + str(idx) + ".pt"))
+            idx += 1
+
+    def len(self):
+        return len(self.processed_file_names)
+
+    def get(self, idx):
+        data = torch.load(osp.join(self.processed_dir,
+                                   "data_" + str(idx) + ".pt"))
+        return data
+
+exit()
+custom_inmemory_dataset = CustomInMemoryDataset(
+    root=dataset_root + "custom_inmemory_example",
+    transform=T.RandomTranslate(0.01),
+    pre_transform=T.KNNGraph(k=6))
+
+custom_dataset = CustomDataset(
+    root=dataset_root + "custom_example",
+    transform=T.RandomTranslate(0.01),
+    pre_transform=T.KNNGraph(k=6))
 
